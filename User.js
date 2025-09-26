b704bf2cdc350083677a36b6eeb6df41f150b1a4
khisa-paul <khisapaul@gmail.com>
2025-09-26 11:24:08 +0300

    Update User.js
    update

--- User.js
+++ User.js
@@ -1,33 +1,8 @@
-// models/User.js
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-    },
-    password: {
-      type: String,
-      required: true,
-      minlength: 6,
-    },
-    avatar: {
-      type: String, // URL to profile picture
-      default: "",
-    }
-  },
-  { timestamps: true }
-);
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
 
-const User = mongoose.model("User", userSchema);
-
-export default User;
+export default mongoose.model("User", userSchema);
